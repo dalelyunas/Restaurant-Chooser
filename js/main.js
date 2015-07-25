ba419b4d1683edc4728be894304d4a4e979e5640
dalelyunas <alelyunas.d@husky.neu.edu>
2015-07-25 13:38:08 -0400

    finalized design of main page of website

---
+++ js/main.js
@@ -0,0 +1,17 @@
+//Array of images which you want to show: Use path you want.
+var images=new Array('url(img/bento.jpg)','url(img/bolognese_meal.jpg)','url(img/cooked_salmon.jpg)', 'url(img/dessert.jpg)');
+var nextimage=0;
+
+function doSlideshow() {
+	if(nextimage>=images.length){ 
+		nextimage=0; 
+	}
+
+    $('.body-container').css('background-image', images[nextimage++]);
+    setTimeout(doSlideshow,5000);
+}
+
+
+$( document ).ready(function() {
+	doSlideshow();
+});
