f9a9c1de163864576fac1af53faa16ceb5ca7694
dalelyunas <alelyunas.d@husky.neu.edu>
2015-07-26 22:40:55 -0400

    started angularjs integration, added module, controller, added features to  search box.

--- js/main.js
+++ js/main.js
@@ -1,17 +1,32 @@
 //Array of images which you want to show: Use path you want.
-var images=new Array('url(img/bento.jpg)','url(img/bolognese_meal.jpg)','url(img/cooked_salmon.jpg)', 'url(img/dessert.jpg)');
-var nextimage=0;
+var images = new Array("url(img/bento.jpg)","url(img/bolognese_meal.jpg)","url(img/cooked_salmon.jpg)", "url(img/dessert.jpg)");
+var nextimage = 0;
+
+var questions = 0;
+var curQuestion = 0;
 
 function doSlideshow() {
-	if(nextimage>=images.length){ 
-		nextimage=0; 
+	if(nextimage >= images.length){ 
+		nextimage = 0; 
 	}
 
-    $('.body-container').css('background-image', images[nextimage++]);
-    setTimeout(doSlideshow,5000);
+    $(".body-container").css("background-image", images[nextimage++]);
+    setTimeout(doSlideshow, 5000);
 }
 
 
-$( document ).ready(function() {
+$(document).ready(function() {
 	doSlideshow();
 });
+
+var app = angular.module("App", []);
+
+app.controller("SearchController", ['$scope', function($scope) {
+	var vm = this;
+	vm.title="What type of food do you want to eat?";
+	vm.button="NEXT";
+	vm.updateQuestion = function() {
+		$scope.inputForm.$setPristine();
+		$scope.searchBox = "";
+	};
+}]);
